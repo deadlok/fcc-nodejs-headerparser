a6e5d762c8bccf56fe7daa5753826e4818fdaae0
man lok lee <lok@pg95.com>
2024-04-05 10:53:52 +0000

    completed

--- index.js
+++ index.js
@@ -17,12 +17,25 @@ app.use(express.static('public'));
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
+  //console.log(req.headers)
+
 });
 
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.get('/api/whoami', function(req, res){
+  const ipaddress = req.headers['x-forwarded-for']
+  const language = req.headers['accept-language']
+  const software = req.headers['user-agent']
+  res.json({
+    ipaddress: ipaddress,
+    language: language,
+    software: software
+  })
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
